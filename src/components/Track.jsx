3fd19abc6715823c1e06e2c382e3c2f65f5f357f
SUARAV KUMAR SINGH <120924610+sauravkumarOm@users.noreply.github.com>
2025-02-08 17:08:57 +0530

    Update Track.jsx

--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -1,27 +1,24 @@
 import React from "react";
 import bg from "../assets/image03.png";
-// import impactX from "../assets/impact-x.png";
-// import outlierQuest from "../assets/outlier-quest.png";
-// import visionaryTracker from "../assets/visionary-tracker.png";
-// import pixelPlay from "../assets/pixel-play.png";
+import impactx from "../assets/impact-x.png";
+import visionTracker from "../assets/visionarytracker.svg";
+import outliner from "../assets/outlierquest.svg"
+import console from "../assets/console.svg"
 
 const tracks = [
-  { name: "IMPACT-X", image: "" },
-  { name: "OUTLIER QUEST", image: "" },
-  { name: "VISIONARY TRACKER", image: "" },
-  { name: "PIXEL PLAY", image: "" },
+  { name: "IMPACT-X", image: impactx },
+  { name: "OUTLIER QUEST", image: outliner },
+  { name: "VISIONARY TRACKER", image: visionTracker },
+  { name: "PIXEL PLAY", image: console },
 ];
 
 const Tracks = () => {
   return (
-    <div
-      className="relative min-h-screen bg-cover bg-center flex flex-col items-center justify-center px-6 py-12"
-      style={{ backgroundImage: `url(${bg})` }}
-    >
+    <div className="relative w-screen min-h-screen flex flex-col items-center justify-center py-12 bg-cover bg-center bg-no-repeat tracks-bg">
       {/* Title */}
-      <h1 className="text-white text-6xl font-extrabold mb-44 tracking-wider squid-font">trAcKs</h1>
-
-      {/* Track Cards */}
+      <h1 className="text-white text-6xl font-extrabold mb-44 tracking-wider squid-font">
+        trAcKs
+      </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 w-full max-w-7xl cursor-pointer">
         {tracks.map((track, index) => (
           <div
@@ -30,8 +27,14 @@ const Tracks = () => {
                   transform hover:scale-110 hover:-translate-y-1 
                   active:scale-95 active:translate-y-1 hover:bg-opacity-20 w-64 h-80"
           >
-            <img src={track.image} alt={track.name} className="w-36 h-36 mb-6 object-contain" />
-            <h2 className="text-white text-2xl font-semibold tracking-wide">{track.name}</h2>
+            <img
+              src={track.image}
+              alt={track.name}
+              className="w-36 h-36 mb-6 object-contain"
+            />
+            <h2 className="text-white text-2xl font-semibold tracking-wide">
+              {track.name}
+            </h2>
           </div>
         ))}
       </div>
